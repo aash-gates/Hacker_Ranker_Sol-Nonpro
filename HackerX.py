8a15c67eb9823c754e89977c5161c94bb87c0221
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-29 15:03:13 +0530

    Update HackerX.py

--- HackerX.py
+++ HackerX.py
@@ -1,2 +1,20 @@
 #Hacker Rank Challenge 11
 
+n = int(input())
+inp = (map(int, input().split()) for i in range(n))
+p = sorted(list((a + b, a - b) for a, b in inp))
+a = list(y for x, y in p)
+d = []
+for x in a:
+    low, high = -1, len(d) # >, <=
+    while high - low > 1:
+        mid = (low + high) >> 1
+        if d[mid] > x:
+            low = mid
+        else:
+            high = mid
+    if high == len(d):
+        d.append(x)
+    else:
+        d[high] = x
+print(len(d))
