b02f01e0ba382a1885167657f5bd557b55c4757d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-29 11:48:06 +0530

    Update Diagonal Difference.py

--- Diagonal Difference.py
+++ Diagonal Difference.py
@@ -1,16 +1,8 @@
 # Hacker Rank Challenge 5
 
-n = input()
-array=[]
-for _ in range(n):
- temp_ar = map(int,input().split())
-
-array.append(temp_ar)
-s1,s2 = 0,0
-for i in range(n):
- s1 += array[i][i]
- s2 += array[-i-1][i]
-
-print (abs(s1-s2))
+def diagonalDifference(arr):
+    d1 = sum([arr[x][x] for x in range(len(arr))])
+    d2 = sum([arr[x][n - 1 - x] for x in range(len(arr))])
+    return(abs(d1 - d2))
 
 # end of the program
