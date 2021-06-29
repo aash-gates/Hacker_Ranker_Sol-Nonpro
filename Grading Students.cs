af93c0b82f4edfaa0e839c2f9e38c71da3ee2c1a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-29 20:49:09 +0530

    rename

---
+++ Grading Students.cs
@@ -0,0 +1,23 @@
+# Hacker Rank Challenge 12
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+static void Main(String[] args) {
+int n = Convert.ToInt32(Console.ReadLine());
+for(int a0 = 0; a0 < n; a0++){
+int grade = Convert.ToInt32(Console.ReadLine());
+int final = 0;
+int next = ((grade / 5) + 1) * 5;
+if (next - grade >= 3 || grade < 38)
+final = grade;
+else
+final = next;
+Console.WriteLine(final);
+
+}
+}
+}
