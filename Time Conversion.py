8b317ad37fe1b68608a3d3dc88943a572f8bc1b5
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-29 14:30:29 +0530

    Update Time Conversion.py

--- Time Conversion.py
+++ Time Conversion.py
@@ -1,2 +1,18 @@
 # Hacker Rank Challenge 9
+#Time Conversion
 
+import re
+time = input()
+times = re.search(r"(?P<hh>..):(?P<mm>..):(?P<ss>..)(?P<ampm>..)", time)
+hour = times.group('hh')
+if times.group('ampm') == 'PM':
+    if hour == '12':
+        hr = '12'
+    else:
+        hr = str(int(hour) + 12)
+else:
+    if hour == '12':
+        hr = '00'
+    else:
+        hr = hour
+print(hr + ':' + times.group('mm') + ':' + times.group('ss'))
