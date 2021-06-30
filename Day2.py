1c12a2d684bf93a38cfd843d5719577a5ad391d4
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-30 23:28:21 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+
+    if __name__ == '__main__':
+        meal_cost = float(input())
+
+        tip_percent = int(input())
+
+        tax_percent = int(input())
+
+        tip = (meal_cost * tip_percent) / 100
+        tax = (meal_cost * tax_percent) / 100
+        totalCost = int(round(meal_cost + tip + tax))
+        print(totalCost)
+
+
+    solve(meal_cost, tip_percent, tax_percent)
