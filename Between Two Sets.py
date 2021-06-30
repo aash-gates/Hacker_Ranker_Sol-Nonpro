d6c7c70e1f14046c896a75eb1cb0ffadb2d62f0f
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-30 20:11:50 +0530

    Update Between Two Sets.py

---
+++ Between Two Sets.py
@@ -0,0 +1,16 @@
+aa,bb=0,0
+n,m=map(int,raw_input().split())
+a=map(int,raw_input().split())
+b=map(int,raw_input().split())
+ct=0
+for i in xrange(max(a),min(b)+1):
+for j in a:
+if i%j!=0:
+break
+else:
+for k in b:
+if k%i!=0:
+break
+else:
+ct+=1
+print ct
