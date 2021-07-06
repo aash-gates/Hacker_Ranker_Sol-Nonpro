0aac500689fe8a83667e2743af6eaf5c4e315020
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-06 11:14:39 +0530

    Update Picking Numbers.py

---
+++ Picking Numbers.py
@@ -0,0 +1,13 @@
+import sys
+
+
+n = int(input().strip())
+a = list(map(int,input().strip().split(' ')))
+
+a.sort()
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if abs(a[j] - a[i]) <= 1:
+            ans = max(ans, j - i + 1)
+print(ans)
