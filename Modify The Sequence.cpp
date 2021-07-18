f82e03beea8d30915b81da21ac40c0b30000fa75
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-18 08:18:18 +0530

    Update Modify The Sequence.cpp

--- Modify The Sequence.cpp
+++ Modify The Sequence.cpp
@@ -1,16 +1,45 @@
-n = int(input())
-l = list(map(int,input().split()))
-
-
-
-for i in range(n-1):
-    if l[i]<=l[i+1]:
-        l[i+1] = l[i+1]-l[i]
-    else:
-        print('NO')
-        break
-else:
-    if l[n-1]==0:
-        print('YES')
-    else:
-        print('NO')
+#include <cstdio>
+#include <cstring>
+using namespace std;
+const int inf = 2000000000;
+int a[1000005];
+int longest;
+    
+void find(int x) {   //a[i] the last element of non-decreasing subsequence of length i. Note that a[i] is increasing. So we can binary search in it to find a position of the new element x. O(logn)
+int left = 1, right = longest;
+    while (left <= right) {
+    
+        int mid = (left + right) >> 1;
+        if (a[mid] <= x) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    a[++right] = x;
+    if (right > longest) {
+        longest =right;
+    }
+}
+    
+int main() {
+int n;
+    scanf("%d",&n);
+    for (int i = 0; i <= n; ++i) {
+        a[i] = inf;
+    }
+    longest = 0; 
+    // we need to find a longest non-decreasing subsequence of the new sequence x[i] - i after elminating all the negatvie values. The famous LIS (longest increasing subsequence problem has a classic O(nlogn) solution 
+    for (int i = 1; i <= n; ++i) {        
+        int x;    
+        scanf("%d",&x);
+        if ((x -= i) >= 0) {
+            find(x);
+        }
+    }
+    printf("%d\n", n - longest);
+    return 0;
+    
+    
+}
