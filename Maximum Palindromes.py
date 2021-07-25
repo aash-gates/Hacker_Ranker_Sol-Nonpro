bafcb4d5e41d069995617c7aa87d8d53ba7f9e46
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-25 20:39:38 +0530

    Update Maximum Palindromes.py

--- Maximum Palindromes.py
+++ Maximum Palindromes.py
@@ -1,7 +1,7 @@
-from collections import Counter # 콜렉션에서 불러옵니다.
+from collections import Counter # Load from collection.
 from math import factorial
 
-# 홀수개의 2로 나눈 나머지들의 개수
+# Number of remaining divided by odd number of 2s
 s = input()
 for _ in range(int(input())):
     l, r = map(int, input().split())
@@ -15,7 +15,7 @@ for _ in range(int(input())):
     for i in count_s:
         sum_of_even_numbers_factorial *= factorial(count_s[i]//2)
         sum_of_even_number += count_s[i]//2
-        if count_s[i]%2 == 1 :# 홀수인것은 더해줘야한다.
+        if count_s[i]%2 == 1 :# Odd should be added.
             odd_number += 1
 
     ans = 0
