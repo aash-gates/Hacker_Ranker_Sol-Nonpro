ad199de6755737936c6e12be4b171089eaadeb42
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-13 12:16:29 +0530

    Update Jogging Cats.pp

--- Jogging Cats.pp
+++ Jogging Cats.pp
@@ -71,3 +71,67 @@ begin
         end;
     end;
 end;
+procedure work(x:longint);
+var
+    k,kk,i,j:longint;
+begin
+    k:=l[x];
+    while k<>0 do
+    begin
+        kk:=l[f[k,1]];
+        while kk<>0 do
+        begin
+            ans:=ans+sum[f[kk,1]];
+            inc(sum[f[kk,1]]);
+            kk:=f[kk,2];
+        end;
+        k:=f[k,2];
+    end;
+    ans:=ans-(sum[x]*(sum[x]-1)div 2);
+    k:=l[x];
+    while k<>0 do
+    begin
+        kk:=l[f[k,1]];
+        while kk<>0 do
+        begin
+            sum[f[kk,1]]:=0;
+            kk:=f[kk,2];
+        end;
+        k:=f[k,2];
+    end;
+end;
+procedure qsort(i,j:longint);
+var
+    l,r,m:longint;
+begin
+    l:=i;r:=j;m:=h[(i+j)div 2];
+    repeat
+        while h[i]>m do inc(i);
+        while h[j]<m do dec(j);
+        if i<=j then
+        begin
+            h[0]:=h[i];h[i]:=h[j];h[j]:=h[0];
+            w[0]:=w[i];w[i]:=w[j];w[j]:=w[0];
+            inc(i);dec(j);
+        end;
+    until i>j;
+    if i<r then qsort(i,r);
+    if l<j then qsort(l,j);
+end;
+begin
+    readln(n,m);
+    p:=1;
+    begin
+        add(u,v);add(v,u);
+    end;
+    j:=0;
+    begin
+        inc(j);bfs[j]:=i;
+    i:=0;
+    for t:=1 to n do h[t]:=num[t];
+    qsort(1,n);
+    begin
+        if num[w[t]]<>0 then
+    end;
+    writeln(ans);
+end.
