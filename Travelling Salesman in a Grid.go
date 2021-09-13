2e7edc1410cec49ee971220873a13a2d478cb438
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-13 11:30:50 +0530

    Update Travelling Salesman in a Grid.go

--- Travelling Salesman in a Grid.go
+++ Travelling Salesman in a Grid.go
@@ -85,5 +85,7 @@ func newConnections(rows, cols, r, c int, cs []cell, conn []byte, w int) ([][]by
 	cell := cs[cellIdx]
 
 	if !fromTop && !fromLeft {
+
+		// No connections leading to current cell.
 	run(os.Stdin, os.Stdout)
 }
