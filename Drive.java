eea755e6f3dc57f7e5b72b891d64e3fccbbf8fbe
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-13 12:00:56 +0530

    Update Drive.java

--- Drive.java
+++ Drive.java
@@ -8,3 +8,12 @@ public class Drive {
 
     public static void main(String[] args)  {
         Scanner scan = new Scanner(System.in);
+        
+        step[] steps = new step[scan.nextInt()];
+        passenger[] passengers = new passenger[scan.nextInt()];
+        int nitro = scan.nextInt();
+        
+        loadStuff(scan,steps,passengers);
+        addPassengers(steps,passengers);
+        calcDepartures(steps);
+        
