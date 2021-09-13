635742756360111c3393ca8ba7691d8fb222da93
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-13 12:00:49 +0530

    Update Drive.java

--- Drive.java
+++ Drive.java
@@ -3,3 +3,8 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
+public class Drive {
+
+    public static void main(String[] args)  {
+        Scanner scan = new Scanner(System.in);
