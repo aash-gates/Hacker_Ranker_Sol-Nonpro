e5c22c25a9d2862d4f3fad6897acdcef75022322
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-14 11:16:24 +0530

    Update DFS Edges.cpp

--- DFS Edges.cpp
+++ DFS Edges.cpp
@@ -6,3 +6,5 @@ using namespace std;
 #define eni(x) sim > typename \
   enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
 sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
