e6a93d4fc4932412ba75145a7cde864ae18419d4
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-15 19:16:50 +0530

    Update Luck Balance.php

--- Luck Balance.php
+++ Luck Balance.php
@@ -2,4 +2,5 @@
 list($n, $k) = array_map("intval", explode(" ", trim(fgets(STDIN))));
 $contests = array();
 for ($i = 0; $i < $n; $i++) {
+  $contests[] = array_map("intval", explode(" ", trim(fgets(STDIN))));
 }
