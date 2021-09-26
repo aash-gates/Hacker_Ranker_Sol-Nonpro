a92e488f720665d38f24e0a473ff633e3b7b32fd
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-26 23:29:54 +0530

    Update Mark and Toys.php

--- Mark and Toys.php
+++ Mark and Toys.php
@@ -6,3 +6,4 @@ $_fp = fopen("php://stdin", "r");
     $N = (int)$data[0];
     $K = (int)$data[1];
     $line_of_text = fgets($_fp);
+    $toysPrices = array_map('intval', explode(' ', $line_of_text));  
