fa9d7ee7d5d71499e5121546a8b1ed2f593570be
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-26 23:23:36 +0530

    Update Candies.rb

--- Candies.rb
+++ Candies.rb
@@ -3,3 +3,4 @@ line = Array.new(n).map{|i| gets.to_i}.insert(0,0).push(0)
 candies1 = Array.new(n,1).insert(0,0).push(0)
 candies2 = Array.new(n,1).insert(0,0).push(0)
 candies1[1] = 1
+for i in 2..n
