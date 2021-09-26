48ae315782c7e8bdcfff56c298074b3e3d8ded03
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-26 23:29:44 +0530

    Update Mark and Toys.php

--- Mark and Toys.php
+++ Mark and Toys.php
@@ -2,3 +2,4 @@
 $_fp = fopen("php://stdin", "r");
 /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     $line_of_text = fgets($_fp);
+    $data = explode(' ', $line_of_text);
