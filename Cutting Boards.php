b65206d596225e30068dc94b54605fedf00ec9aa
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-27 23:32:31 +0530

    Update Cutting Boards.php

--- Cutting Boards.php
+++ Cutting Boards.php
@@ -27,3 +27,4 @@ for($z=0;$z<$T;$z++){
             if($Y[sizeof($Y)-1]>$X[sizeof($X)-1]){
                 $cy++;
                 $count+=$cx*$Y[sizeof($Y)-1];
+                unset($Y[sizeof($Y)-1]);
