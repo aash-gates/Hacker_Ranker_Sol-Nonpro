29cb4766e19ee9f9d35afacc26a77b4c886f906e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-27 23:38:32 +0530

    Update Permuting Two Arrays.php

--- Permuting Two Arrays.php
+++ Permuting Two Arrays.php
@@ -3,4 +3,5 @@ $_fp = fopen("php://stdin", "r");
 $tests = intval(fgets($_fp));
 for( $i = 0; $i < $tests; $i++ )
 {
+    list($cnt, $K) = explode(' ', fgets($_fp));
     $a = array_map('intval', explode(' ', fgets($_fp)));
