d79bc4501b79b76cc88c1bed6d69e139c011a600
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-10-10 23:44:14 +0530

    Update A Very Big Sum.py

--- A Very Big Sum.py
+++ A Very Big Sum.py
@@ -1,1 +1,11 @@
 #Hacker Rank Challenge 4
+import math
+import sys
+ans = 0
+for linenum, testnum in enumerate(sys.stdin):
+    if linenum == 1:
+        for num in testnum.split():
+            ans += int(num)
+print(ans)
+
+#end of the program
